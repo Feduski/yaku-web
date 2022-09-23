2ecfb6271c5b13aefbcd13c3551d84b45a0ce1d1
Federico <fedengp@gmail.com>
2022-09-23 08:27:49 -0300

    fix: removed height value from page and flask

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from pymongo import MongoClient
 from flask import Flask, render_template
 import os
 
-mongo_user = os.getenv('mongo_user')#importing our secret key
+MONGO_URI = os.getenv('mongo_user')#importing our secret key
      
-client = MongoClient(mongo_user) #here we import the mongo client
+client = MongoClient(MONGO_URI) #here we import the mongo client
  
 mydata = list(client.YakuNodos['Nodos'].find()) #searching for the info
 lenght = len(mydata) - 1 #selecting the last one
@@ -19,8 +19,8 @@ def hello_world():
     return render_template('home.html', soil_m=last_callback['Soil Moisture'], 
         ldr=last_callback['LDR'], press=last_callback['Pressure'], 
         hum=last_callback['Humidity'], temp=last_callback['Temperature'], 
-        height=last_callback['Height'], relay = last_callback['Relay'],
-        wind=last_callback['Wind']) #sending the parameters to the page
+        relay = last_callback['Relay'], wind=last_callback['Wind']) #sending the parameters to the page
+        #height=last_callback['Height']
 
 if __name__ == "__main__":
     print(last_callback)
